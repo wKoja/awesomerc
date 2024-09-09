d94f0011f5d89ff245328f7180b4a95d8fa4c493
wKoja <cfwilliam.ferreira@gmail.com>
2024-09-09 08:24:55 -0300

    slack and thunderbird rules

--- configuration/client/rules.lua
+++ configuration/client/rules.lua
@@ -1,70 +1,82 @@
-local awful = require('awful')
-local gears = require('gears')
-local client_keys = require('configuration.client.keys')
-local client_buttons = require('configuration.client.buttons')
-local title_bars = require('configuration.client.titlebars')
+local awful = require("awful")
+local gears = require("gears")
+local client_keys = require("configuration.client.keys")
+local client_buttons = require("configuration.client.buttons")
+local title_bars = require("configuration.client.titlebars")
 local beautiful = require("beautiful")
 
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = beautiful.border_focus,
-                     raise = true,
-                     floating = false,
-                     size_hints_honor = false,
-                     keys = client_keys,
-                     buttons = client_buttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
+  -- All clients will match this rule.
+  {
+    rule = {},
+    properties = {
+      border_width = beautiful.border_width,
+      border_color = beautiful.border_normal,
+      focus = beautiful.border_focus,
+      raise = true,
+      floating = false,
+      size_hints_honor = false,
+      keys = client_keys,
+      buttons = client_buttons,
+      screen = awful.screen.preferred,
+      placement = awful.placement.no_overlap + awful.placement.no_offscreen,
     },
+  },
 
-    -- Floating clients.
-    { rule_any = {
-        instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-          "pinentry",
-          "spterm",
-          "spcalc",
-          "bg"
-        },
-        class = {
-          "Arandr",
-          "Blueman-manager",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-          "Wpa_gui",
-          "veromix",
-          "xtightvncviewer"},
+  -- Floating clients.
+  {
+    rule_any = {
+      instance = {
+        "DTA", -- Firefox addon DownThemAll.
+        "copyq", -- Includes session name in class.
+        "pinentry",
+        "spterm",
+        "spcalc",
+        "bg",
+      },
+      class = {
+        "Arandr",
+        "Blueman-manager",
+        "Gpick",
+        "Kruler",
+        "MessageWin", -- kalarm.
+        "Sxiv",
+        "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
+        "Wpa_gui",
+        "veromix",
+        "xtightvncviewer",
+      },
 
-        -- Note that the name property shown in xprop might be set slightly after creation of the client
-        -- and the name shown there might not match defined rules here.
-        name = {
-          "Event Tester",  -- xev.
-        },
-        role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "ConfigManager",  -- Thunderbird's about:config.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      }, properties = { floating = true }},
-
-    -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = false }
+      -- Note that the name property shown in xprop might be set slightly after creation of the client
+      -- and the name shown there might not match defined rules here.
+      name = {
+        "Event Tester", -- xev.
+      },
+      role = {
+        "AlarmWindow", -- Thunderbird's calendar.
+        "ConfigManager", -- Thunderbird's about:config.
+        "pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
+      },
     },
+    properties = { floating = true },
+  },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    { rule = { class = "DataGrip" },
-      properties = { screen = 2, tag = "2" } },
+  -- Add titlebars to normal clients and dialogs
+  {
+    rule_any = { type = { "normal", "dialog" } },
+    properties = { titlebars_enabled = false },
+  },
+
+  -- Set Datagrip to always map on the tag named "2" on screen 1.
+  {
+    rule = { class = "Slack" },
+    properties = { screen = 1, tag = "3" },
+  },
+  {
+    rule = { class = "thunderbird" },
+    properties = { screen = 1, tag = "8" },
+  },
 }
 -- }}}
-
