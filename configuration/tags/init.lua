6758557dab0d73eff7c07dca04a74576229f6c9b
wKoja <cfwilliam.ferreira@gmail.com>
2024-09-09 08:41:50 -0300

    selene config

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -1,12 +1,9 @@
-local awful = require('awful')
-local gears = require('gears')
--- local icons = require('theme.icons')
-local apps = require('configuration.apps')
+local awful = require("awful")
 
 -- Configure Tag Properties
 awful.screen.connect_for_each_screen(function(s)
-    -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+  -- Each screen has its own tag table.
+  awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 end)
 -- }}}
 
@@ -27,4 +24,3 @@ awful.layout.layouts = {
   -- awful.layout.suit.corner.se,
 }
 -- }}}
-
