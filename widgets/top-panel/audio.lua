8d19beb4e10ec307d2f7d3636671f1427b03ad80
wKoja <cfwilliam.ferreira@gmail.com>
2024-09-23 08:32:06 -0300

    using pulse audio widget

--- widgets/top-panel/audio.lua
+++ widgets/top-panel/audio.lua
@@ -1,14 +1,29 @@
 local lain = require("lain")
-local beautiful = require("beautiful")
+local awful = require("awful")
 
-local markup = lain.util.markup
-
-volume = lain.widget.alsa({
-  timeout = 1,
+volume = lain.widget.pulse({
+  timeout = 0.1,
   settings = function()
-    local status = volume_now.status
-    local vol = volume_now.level
-    if status == "off" then vol = "muted" end
-    widget:set_markup(markup.fontfg(beautiful.font, beautiful.blue, " vol " .. vol .. " "))
+    vlevel = " vol " .. volume_now.left .. "% "
+    if volume_now.muted == "yes" then vlevel = vlevel .. " M " end
+    widget:set_markup(lain.util.markup("#7493d2", vlevel))
   end,
 })
+
+volume.widget:buttons(awful.util.table.join(
+  awful.button({}, 1, function() -- left click
+    awful.spawn("pavucontrol")
+  end),
+  awful.button({}, 3, function() -- right click
+    os.execute(string.format("pactl set-sink-mute %s toggle", volume.device))
+    volume.update()
+  end),
+  awful.button({}, 4, function() -- scroll up
+    os.execute(string.format("pactl set-sink-volume %s +1%%", volume.device))
+    volume.update()
+  end),
+  awful.button({}, 5, function() -- scroll down
+    os.execute(string.format("pactl set-sink-volume %s -1%%", volume.device))
+    volume.update()
+  end)
+))
